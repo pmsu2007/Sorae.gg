202bafe641e03e7fd684b95e6e5b99503f6de8ca
unknown <pmsu2007@naver.com>
2021-10-31 18:40:46 +0900

    refactor: modify Primary Key

--- summoner/models.py
+++ summoner/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Tier(models.Model):
-    summoner_name = models.CharField(max_length=20)
+    summoner_name = models.CharField(max_length=20, primary_key=True)
     solo_tier = models.CharField(max_length=20)
     solo_rank = models.CharField(max_length=20)
     free_tier = models.CharField(max_length=20)
