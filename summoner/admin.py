ce366b863ff49d55d5ea15e49f06efec34e5f372
unknown <pmsu2007@naver.com>
2022-01-19 02:17:10 +0900

    register User, GameRecord model

--- summoner/admin.py
+++ summoner/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Tier
+from .models import Tier, User, GameRecord
 
 admin.site.register(Tier)
+admin.site.register(User)
+admin.site.register(GameRecord)
 # Register your models here.
