90bf76a51460c966d6b010dbe7cabd98d4257c71
김남주 <kimnamju@gimnamjuui-MacBookPro.local>
2022-01-18 19:11:25 +0900

    add base template html

--- summoner/urls.py
+++ summoner/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include, path
 from django.contrib import admin
-from summoner.views import TierListAPI
+# from summoner.views import TierListAPI
+from summoner import views
 
 
 urlpatterns = [
-    path('', TierListAPI.as_view())
+    path('', views.search)
 ]
