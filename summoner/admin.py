f3a12d408541ab27593cb01a1edc2f3666249086
unknown <pmsu2007@naver.com>
2022-01-19 22:21:15 +0900

    add DetailRecord to admin

--- summoner/admin.py
+++ summoner/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Tier, User, GameRecord
+from .models import Tier, User, GameRecord, DetailRecord
 
 admin.site.register(Tier)
 admin.site.register(User)
 admin.site.register(GameRecord)
+admin.site.register(DetailRecord)
 # Register your models here.
