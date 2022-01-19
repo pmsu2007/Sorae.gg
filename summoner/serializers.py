7dbe07172b25db4df29109eb79d76cbf8930da66
unknown <pmsu2007@naver.com>
2022-01-19 11:31:21 +0900

    implement Nested Serializer (UserSerializer)

--- summoner/serializers.py
+++ summoner/serializers.py
@@ -1,14 +1,8 @@
 from rest_framework import serializers
-from .models import Tier, GameRecord, Summoner
+from .models import Tier, GameRecord, User
 
 # Serializer를 선언할 때는 데이터 형태에 대한 필드, 데이터를 처리할 메소드가 필요
 # ModelSerializer는 위의 내용들을 알아서 해준 형태의 Serializer이다.
-
-
-class TierSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tier        # Tier 모델 사용
-        fields = '__all__'  # 모든 필드 포함
 
 
 class GameRecordSerializer(serializers.ModelSerializer):
@@ -17,7 +11,17 @@ class GameRecordSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class SummonerSerializer(serializers.ModelSerializer):
+class TierSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Summoner
+        model = Tier
+        fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    record = GameRecordSerializer(many=True, read_only=True)
+    tier = TierSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = User
         fields = '__all__'
