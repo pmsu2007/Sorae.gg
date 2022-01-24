fae9ef880582030498448ccd2c44ad7ab387a35e
unknown <pmsu2007@naver.com>
2022-01-24 17:22:23 +0900

    delete main path & modify summoner path

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
-    path('summoner/', include('summoner.urls'))
+    # path('', include('main.urls')),
+    path('', include('summoner.urls'))
 ]
