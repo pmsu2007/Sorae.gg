28b49da4debca6e154587d476389486559caf435
unknown <pmsu2007@naver.com>
2022-02-18 12:51:06 +0900

    Add Question Answer

--- community/urls.py
+++ community/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'community'
+
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
 ]
--- community/views.py
+++ community/views.py
@@ -1,8 +1,42 @@
-from django.shortcuts import render
-from .models import Question
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import generic
+from django.utils import timezone
+from .models import Question, Answer
 
 
 def index(request):
     question_list = Question.objects.order_by('-create_date')
     context = { 'question_list': question_list }
-    return render(request, 'pybo/question_list.html', context)
+    return render(request, 'community/question_list.html', context)
+
+
+def detail(request, question_id):
+    """
+    detail 내용 출력
+    """
+    question = Question.objects.get(id=question_id)
+    context = {'question': question}
+    return render(request, 'community/question_detail.html', context)
+
+
+def answer_create(request, question_id):
+
+    question = get_object_or_404(Question, pk=question_id)
+    answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
+    answer.save()
+    return redirect('pybo:detail', question_id=question.id)
+
+
+class IndexView(generic.ListView):
+    """
+    pybo 목록 출력
+    """
+    def get_queryset(self):
+        return Question.objects.order_by('-create_date')
+
+
+class DetailView(generic.DetailView):
+    """
+    pybo 내용 출력
+    """
+    model = Question
